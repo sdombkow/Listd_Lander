f2e01a145074c755dfb9b241e0c026d4cc28ddc3
Rohan Kshirsagar <rohan.kshirsagar@duke.edu>
2012-12-01 23:33:40 -0500

    added seed file. check seeds.rb for admin account info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'Listd Admin', :email => 'admin@listdnow.com', :password => 'adminpw', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user.admin = true
+user.save
+user2 = User.create! :name => 'First partner', :email => 'partner@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user2.name
+user2.partner = true
+user2.save
+user3 = User.create! :name => 'First User', :email => 'user3@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user3.name
