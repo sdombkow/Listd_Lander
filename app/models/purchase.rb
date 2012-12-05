a4e8e5e0756d5e2b5dac94c3f76e938c7eeac38c
Scott Dombkowski <sdombkow@andrew.cmu.edu>
2012-12-05 18:08:11 -0500

    Full Stripe Functionality at Checkout w/o Verifications

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -21,6 +21,22 @@ class Purchase < ActiveRecord::Base
         false
     end
     
+    def payment_return_customer(user)
+      if valid?
+        logger.error "Stripe Card Token: #{name} and #{stripe_card_token}"
+        charge = Stripe::Charge.create(
+          :amount => 1000,
+          :currency => "usd",
+          :customer => user.stripe_customer_token
+        )
+        save!
+      end
+      rescue Stripe::InvalidRequestError => e
+          logger.error "Stripe error while creating customer: #{e.message}"
+          errors.add :base, "There was a problem with your credit card."
+          false
+      end
+    
     def save_with_payment(user)
       if valid?
         logger.error "Stripe Card Token: #{name} and #{stripe_card_token}"
@@ -29,6 +45,7 @@ class Purchase < ActiveRecord::Base
           :description => name
         )
         user.stripe_customer_token = customer.id
+        user.save
         charge = Stripe::Charge.create(
           :amount => 1000,
           :currency => "usd",
