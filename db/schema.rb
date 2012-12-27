35649fa12f6fbdce4facbc0d1dddc238d3846787
Michael Wang <mwang4@andrew.cmu.edu>
2012-12-27 17:24:33 -0500

    Initial

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,8 @@ ActiveRecord::Schema.define(:version => 20121223134117) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.string   "name"
-    t.boolean  "admin",                  :default => false
-    t.boolean  "partner",                :default => false
+    t.boolean  "admin",                  :default => false, :null => false
+    t.boolean  "partner",                :default => false, :null => false
     t.string   "provider"
     t.string   "uid"
     t.string   "stripe_customer_token"
