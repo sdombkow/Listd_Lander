5bbe8f517955b64f463927a4183d882dc8ce5373
Rohan Kshirsagar <rohan.kshirsagar@duke.edu>
2013-01-02 10:58:23 -0500

    testing out different ways to get client ip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,6 +13,9 @@ class HomeController < ApplicationController
     
     #acquires users ip address for geolocating
     @user_info = request.ip
+    @user_info2 = request.remote_addr
+    request.remote_ip
+    @user_info3 = request.env["HTTP_X_FORWARDED_FOR"]
     #@user_info = "98.122.189.233"
     #acquires all the needed information based on the users ip address
     @user_information = Geocoder.search(@user_info)
