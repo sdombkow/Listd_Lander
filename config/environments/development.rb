cbad2baab047731ffc5f4a024f8a2aff9c66628f
Steven Chen <g.naxre@gmail.com>
2013-01-04 14:18:20 -0500

    fixed broken url host local host for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,10 @@ ListdApp::Application.configure do
   :authentication       => "plain",  
   :enable_starttls_auto => true  
   }
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000" 
+  
+config.action_mailer.default_url_options = {
+  :host => "localhost"
+}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
