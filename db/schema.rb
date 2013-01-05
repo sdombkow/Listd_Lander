7d3a00f5a24f3959bdcfc0f6f0fcd7e95891af65
Rohan Kshirsagar <rohan.kshirsagar@duke.edu>
2013-01-05 15:09:51 -0500

    phone number issue fixx

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130104000358) do
+ActiveRecord::Schema.define(:version => 20130105195743) do
 
   create_table "bars", :force => true do |t|
     t.string   "name"
-    t.integer  "phone_number"
+    t.integer  "phone_number",        :limit => 8
     t.string   "address"
     t.text     "intro_paragraph"
     t.integer  "user_id"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.float    "latitude"
     t.float    "longitude"
     t.string   "logo"
